5c8c83bf1c172208470798141a496a5f94662f50
Anthony Farncombe <anthony.farncombe@redpack.co.uk>
2020-10-27 22:18:30 +0000

    HVAC styling and cassette control

--- pages/hmi/hvac.vue
+++ pages/hmi/hvac.vue
@@ -94,6 +94,7 @@
           disable-pagination
           disable-sort
           hide-default-footer
+          :item-class="condenserItemClass"
         >
           <template v-slot:[`item.command`]="{ item }">
             {{
@@ -116,6 +117,8 @@
           <template v-slot:[`item.defrost`]="{ item }">
             {{ (item.value & 0x20) > 0 ? 'Defrosting' : '' }}
           </template>
+
+          <template v-slot:[`item.hours`]> 0 </template>
         </v-data-table>
       </v-tab-item>
 
@@ -130,6 +133,7 @@
                 disable-pagination
                 disable-sort
                 hide-default-footer
+                :item-class="fanItemClass"
               >
                 <template v-slot:[`item.speed`]="{ item }">
                   {{ getFanCommand(item).value }}
@@ -176,9 +180,10 @@
           disable-pagination
           disable-sort
           hide-default-footer
+          :item-class="cassetteItemClass"
         >
           <template v-slot:[`item.command`]="{ item }">
-            {{ (item.value & 0x4) > 0 ? 'Toggle' : 'No Command' }}
+            {{ (item.value & 0x4) > 0 ? 'Requesting' : 'No Command' }}
           </template>
 
           <template v-slot:[`item.running`]="{ item }">
@@ -187,6 +192,12 @@
 
           <template v-slot:[`item.fault`]="{ item }">
             {{ (item.value & 0x10) > 0 ? 'Fault' : 'OK' }}
+          </template>
+
+          <template v-slot:[`item.action`]="{ item }">
+            <v-btn @click="toggleCassette(item)"
+              >Turn {{ (item.value & 0x8) > 0 ? 'Off' : 'On' }}</v-btn
+            >
           </template>
         </v-data-table>
       </v-tab-item>
@@ -215,18 +226,27 @@ export default {
       {
         text: 'Command',
         value: 'command',
+        align: 'center',
       },
       {
-        text: 'Runnung',
+        text: 'Running',
         value: 'running',
+        align: 'center',
       },
       {
         text: 'Fault',
         value: 'fault',
+        align: 'center',
       },
       {
         text: 'Defrost',
         value: 'defrost',
+        align: 'center',
+      },
+      {
+        text: 'Hours',
+        value: 'hours',
+        align: 'center',
       },
     ],
     fanHeaders: [
@@ -237,10 +257,12 @@ export default {
       {
         text: 'Requested Speed',
         value: 'speed',
+        align: 'center',
       },
       {
         text: 'Status',
         value: 'fault',
+        align: 'center',
       },
     ],
     meetingSize: { value: 0 },
@@ -253,14 +275,22 @@ export default {
       {
         text: 'Command',
         value: 'command',
+        align: 'center',
       },
       {
         text: 'Running',
         value: 'running',
+        align: 'center',
       },
       {
         text: 'Fault',
         value: 'fault',
+        align: 'center',
+      },
+      {
+        text: 'Action',
+        value: 'action',
+        align: 'center',
       },
     ],
   }),
@@ -334,6 +364,43 @@ export default {
         name: 'meetingSize',
         value,
       })
+    },
+    toggleCassette(item) {
+      const name =
+        'toggle' +
+        item.name[0].toUpperCase() +
+        item.name.substr(1, item.name.length - 7) +
+        's'
+      this.$store.dispatch('setVariableValue', { name, value: true })
+    },
+    condenserItemClass(item) {
+      if ((item.value & 0x10) > 0) {
+        return 'orange'
+      } else if ((item.value & 0x20) > 0) {
+        return 'blue'
+      } else if ((item.value & 0x8) > 0) {
+        return 'green'
+      } else {
+        return 'red'
+      }
+    },
+    fanItemClass(item) {
+      if (item.value) {
+        return 'orange'
+      } else if (this.getFanCommand(item).value) {
+        return 'green'
+      } else {
+        return 'red'
+      }
+    },
+    cassetteItemClass(item) {
+      if ((item.value & 0x10) > 0) {
+        return 'orange'
+      } else if ((item.value & 0x8) > 0) {
+        return 'green'
+      } else {
+        return 'red'
+      }
     },
   },
   head: {
